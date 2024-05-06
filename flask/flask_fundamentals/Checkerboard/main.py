b4e3925aced113f3ed6ead8364e0ddf5af01538b
Majed Alkhaldi <majed_khaldi@hotmail.com>
2024-05-06 21:14:54 +0300

    update the assignment

--- flask/flask_fundamentals/Checkerboard/main.py
+++ flask/flask_fundamentals/Checkerboard/main.py
@@ -3,20 +3,20 @@ app = Flask(__name__)
     
 @app.route('/')                           
 def check():
-    return render_template('index.html', rows = 4, cols = 4, color1 = "aqua", color2 = "#ce6545")  
+    return render_template('index.html', rows = 8, cols = 8, color1 = "aqua", color2 = "#ce6545")  
 
 @app.route('/<int:x>')                           
 def check4(x):
-    return render_template('index.html', rows = int(x/2), cols = 4, color1 = "aqua", color2 = "#ce6545")  
+    return render_template('index.html', rows = int(x), cols = 8, color1 = "aqua", color2 = "#ce6545")
 
 @app.route('/<int:x>/<int:y>')                           
 def checkxy(x,y):
-    return render_template('index.html', rows = int(x/2) , cols = int(y/2), color1 = "aqua", color2 = "#ce6545")  
+    return render_template('index.html', rows = int(x) , cols = int(y), color1 = "aqua", color2 = "#ce6545")  
 
 
 @app.route('/<int:x>/<int:y>/<c>/<cl>')                           
 def checkxycolors(x,y,c,cl):
-    return render_template('index.html', rows = int(x/2) , cols = int(y/2), color1 = c, color2 = cl) 
+    return render_template('index.html', rows = int(x) , cols = int(y), color1 = c, color2 = cl) 
 
 
 if __name__=="__main__":
